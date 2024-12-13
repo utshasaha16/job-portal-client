8da8c7f74633b9628a16f843bf75f8ac350f8c54
utshasaha16 <utsharoy12345@gmail.com>
2024-12-13 11:52:13 +0600

    animate image in banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { easeOut, motion } from "motion/react"
 import team1 from '../../assets/team/team-1.jpg'
+import team2 from '../../assets/team/team-2.jpg'
 
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className='flex-1'>
-                    <img
+                    <motion.img
                         src={team1}
-                        className="max-w-sm rounded-lg shadow-2xl" />
+                        animate={{y: [50, 100, 50]}}
+                        transition={{duration: 10, repeat: Infinity}}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600 shadow-2xl" />
+                    <motion.img
+                        src={team2}
+                        animate={{x: [100, 150, 100]}}
+                        transition={{duration: 10, delay: 5, repeat: Infinity}}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600 shadow-2xl" />
                 </div>
                 <div className='flex-1'>
                     <motion.h1
